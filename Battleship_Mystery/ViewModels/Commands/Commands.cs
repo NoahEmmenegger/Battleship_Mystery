d8b06e0633e4b5b8ff0f157cb57613657ad6f00c
Nicolas Bürgler <trajan15.nb@gmail.com>
2020-11-06 10:23:48 +0100

    ToolMenu Page fertiggestellt

---
+++ Battleship_Mystery/ViewModels/Commands/Commands.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battleship_Mystery.ViewModels.Commands
+{
+    public class GenerateMysteryCommand : BaseCommand
+    {
+        public GenerateMysteryCommand(Action<object> execute) : base(execute)
+        {
+
+        }
+    }
+
+    public class HelpCommand : BaseCommand
+    {
+        public HelpCommand(Action<object> execute) : base(execute)
+        {
+
+        }
+    }
+
+    public class SaveVirtualMysteryCommand : BaseCommand
+    {
+        public SaveVirtualMysteryCommand(Action<object> execute) : base(execute)
+        {
+
+        }
+    }
+
+    public class LoadVirtualMysteryCommand : BaseCommand
+    {
+        public LoadVirtualMysteryCommand(Action<object> execute) : base(execute)
+        {
+
+        }
+    }
+
+
+}
--- Battleship_Mystery/ViewModels/ToolMenuViewModel.cs
+++ Battleship_Mystery/ViewModels/ToolMenuViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Battleship_Mystery.ViewModels.Commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,31 @@ namespace Battleship_Mystery.ViewModels
 {
     public class ToolMenuViewModel
     {
+        public HelpCommand HelpCommand { get; set; }
+        public SaveVirtualMysteryCommand SaveVirtualMysteryCommand { get; set; }
+        public LoadVirtualMysteryCommand LoadVirtualMysteryCommand { get; set; }
 
+        public ToolMenuViewModel()
+        {
+            HelpCommand = new HelpCommand(GetHelp);
+            SaveVirtualMysteryCommand = new SaveVirtualMysteryCommand(SaveVirtualMystery);
+            LoadVirtualMysteryCommand = new LoadVirtualMysteryCommand(LoadVirtualMystery);
+
+        }
+
+        private void GetHelp(object parameter)
+        {
+
+        }
+
+        private void SaveVirtualMystery(object parameter)
+        {
+
+        }
+
+        private void LoadVirtualMystery(object parameter)
+        {
+
+        }
     }
 }
