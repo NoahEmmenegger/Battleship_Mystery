030b359b102c3cf1be202b0bdbecdb4b68809966
Nicolas Bürgler <trajan15.nb@gmail.com>
2020-12-17 17:48:09 +0100

    Kommentare hinzugefügt

--- Battleship_Mystery/Business/Ship.cs
+++ Battleship_Mystery/Business/Ship.cs
@@ -6,7 +6,13 @@ namespace Battleship_Mystery.Business
 {
     public class Ship
     {
+        /// <summary>
+        /// Die Anzahl Felder, welche das Schiff einnimt.
+        /// </summary>
         public int Size { get; set; }
+        /// <summary>
+        /// Die Felder von diesem Schiff
+        /// </summary>
         public List<Field> Fields { get; set; }
 
         public Ship(int size)
